44ff9b07886a38f054ea0152fa2292e4d2ef3bd0
mohsin027 <itsmohsin343@gmail.com>
2024-03-19 10:47:39 +0530

    updates

--- src/components/UserHome/UserHome.jsx
+++ src/components/UserHome/UserHome.jsx
@@ -5,7 +5,7 @@ import axios from '../../axios'
 import { useDispatch ,useSelector } from 'react-redux';
 
 function UserHome() {
-  const imgURL='http://localhost:4000/upload_img/'
+  const imgURL=process.env.REACT_APP_SERVER_URL+'/upload_img/'
   axios.defaults.withCredentials = true;
 const navigate =useNavigate();
 const dispatch=useDispatch();
